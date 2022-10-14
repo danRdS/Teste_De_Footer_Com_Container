49d334385c4ac3c2b8feac69f3a6c0c885a3f06f
Daniel RIbeiro <105250401+danRdS@users.noreply.github.com>
2022-10-13 22:49:29 -0300

    Projetos Daniel

---
+++ script.js
@@ -0,0 +1,12 @@
+function tema() {
+    document.body.classList.toggle("dark")
+}
+
+function recupera(){
+    console.log("Enviado!")
+    document.getElementById("confirmaEnvio").innerHTML = "Enviado!"
+}
+
+function hover() {
+    document.body.classList.toggle("mostrar")
+}
